076ba624bf95fe6adba1189d246a7b071d0a5572
Nolan Gelinas <nolangelinas@gmail.com>
2021-11-15 22:01:16 -0500

    Fix delete button

--- src/App.js
+++ src/App.js
@@ -3,25 +3,31 @@ import React, {useState} from "react";
 
 function App() {
 	const [todoList, setTodoList] = useState([{
-		title: "Title",
-		description: "Fuck you",
+		title: "Buy Groceries",
+		description: "Lemons\n" +
+			"Mangoes\n" +
+			"Melon\n" +
+			"Nectarines\n" +
+			"Oranges\n" +
+			"Papaya\n" +
+			"Peaches\n" +
+			"Pears\n" +
+			"Pineapple",
 	}, {
-		title: "Todo 2",
-		description: "No fuck you",
+		title: "Do laundry",
+		description: "Complete by 11/16/2021",
 	}]);
 
-	console.log(todoList);
-
 	function removeFromList(index) {
-		const updatedList = [...todoList];
-		updatedList.filter((todo, i) => i !== index);
+		let updatedList = [...todoList];
+		updatedList = updatedList.filter((todo, i) => (i !== index));
 		setTodoList(updatedList);
 	}
 
 	return (
 		<div className="todos">
 			{todoList.map((todo, index) => <Todo title={todo.title} description={todo.description}
-												 onDelete={() => removeFromList(index)}/>)}
+												 onDelete={() => removeFromList(index)} key={index}/>)}
 		</div>
 	);
 }
